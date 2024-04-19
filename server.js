f19a357e728acc6bbcb18301a2192192ffe43cf2
Seth Frank <sbfrank@wpi.edu>
2024-04-19 14:05:45 -0400

    Server set up with express

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-//server.js
+import express from  'express'
+import ViteExpress from 'vite-express'
+
+const app = express()
+
+app.use( express.json() )
+
+app.use( (req,res, next) => {
+  console.log( req.url )
+  next()
+})
+
+ViteExpress.listen( app, 3000 )
