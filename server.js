3b932eb0db532ebaf327bc2f8ebafb6737b5d311
spencer <spencerscottg@gmail.com>
2024-04-22 09:41:09 -0400

    ooooh, its just a little red wine, ill be fine

--- server.js
+++ server.js
@@ -1,13 +1,46 @@
-import express from  'express'
+import express from 'express'
+import passport from 'passport'
 import ViteExpress from 'vite-express'
+import cors from 'cors'
+import session from 'express-session'
+import { passportConfig } from './config/passport.js'
 
 const app = express()
 
-app.use( express.json() )
+app.use(express.json())
+passportConfig(passport);
+app.use(express.json());
+app.use(cors());
+app.use(session({
+	secret: 'sessionSecretHehe',
+	resave: false,
+	saveUninitialized: false,
+	cookie: { maxAge: 60000 * 60 * 24 }
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use( (req,res, next) => {
-  console.log( req.url )
+// generic middleware that can be put on any route where user must first be authenticated
+const ensureAuthenticated = function (req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect("login.html");
+}
+
+app.use((req, res, next) => {
+  console.log(req.url)
   next()
 })
 
-ViteExpress.listen( app, 3000 )
+app.get((req, res) => {
+  console.log(req.url)
+  next()
+})
+
+app.post((req, res) => {
+  console.log(req.url)
+  next()
+})
+
+ViteExpress.listen(app, 3000)
