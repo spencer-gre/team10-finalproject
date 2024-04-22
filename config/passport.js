c21d01364d6927c508e8c9dfefde4a2dc0f8a7c5
Restyled.io <commits@restyled.io>
2024-04-22 13:45:19 +0000

    Restyled by prettier

--- config/passport.js
+++ config/passport.js
@@ -1,33 +1,37 @@
-import dotenv from "dotenv"
-import GithubStrategy from 'passport-github2'
+import dotenv from "dotenv";
+import GithubStrategy from "passport-github2";
 
-dotenv.config()
+dotenv.config();
 
-const passportConfig =
-    (passport) => {
-      passport.serializeUser(function(user, done) { done(null, user); });
+const passportConfig = (passport) => {
+  passport.serializeUser(function (user, done) {
+    done(null, user);
+  });
 
-      passport.deserializeUser(function(obj, done) { done(null, obj); });
+  passport.deserializeUser(function (obj, done) {
+    done(null, obj);
+  });
 
-      passport.use(new GithubStrategy.Strategy(
-          {
-            clientID : process.env.GITHUB_CLIENT_ID,
-            clientSecret : process.env.GITHUB_CLIENT_SECRET,
-            callbackURL : process.env.GITHUB_CALLBACK
-          },
-          function(accessToken, refreshToken, profile, done) {
-            // asynchronous verification, for effect...
-            process.nextTick(function() {
-              // To keep the example simple, the user's GitHub profile is
-              // returned to represent the logged-in user.  In a typical
-              // application, you would want to associate the GitHub account
-              // with a user record in your database, and return that user
-              // instead.
-              return done(null, profile);
-            });
-          }));
-    }
+  passport.use(
+    new GithubStrategy.Strategy(
+      {
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        callbackURL: process.env.GITHUB_CALLBACK,
+      },
+      function (accessToken, refreshToken, profile, done) {
+        // asynchronous verification, for effect...
+        process.nextTick(function () {
+          // To keep the example simple, the user's GitHub profile is
+          // returned to represent the logged-in user.  In a typical
+          // application, you would want to associate the GitHub account
+          // with a user record in your database, and return that user
+          // instead.
+          return done(null, profile);
+        });
+      },
+    ),
+  );
+};
 
-export {
-  passportConfig
-}
+export { passportConfig };
--- server.js
+++ server.js
@@ -1,23 +1,25 @@
-import cors from 'cors'
-import express from 'express'
-import session from 'express-session'
-import passport from 'passport'
-import ViteExpress from 'vite-express'
+import cors from "cors";
+import express from "express";
+import session from "express-session";
+import passport from "passport";
+import ViteExpress from "vite-express";
 
-import {passportConfig} from './config/passport.js'
+import { passportConfig } from "./config/passport.js";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 passportConfig(passport);
 app.use(express.json());
 app.use(cors());
-app.use(session({
-  secret : 'sessionSecretHehe',
-  resave : false,
-  saveUninitialized : false,
-  cookie : {maxAge : 60000 * 60 * 24}
-}));
+app.use(
+  session({
+    secret: "sessionSecretHehe",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60000 * 60 * 24 },
+  }),
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -28,21 +30,21 @@ const ensureAuthenticated = function (req, res, next) {
     return next();
   }
   res.redirect("login.html");
-}
+};
 
 app.use((req, res, next) => {
-  console.log(req.url)
-  next()
-})
+  console.log(req.url);
+  next();
+});
 
 app.get((req, res) => {
-  console.log(req.url)
-  next()
-})
+  console.log(req.url);
+  next();
+});
 
 app.post((req, res) => {
-  console.log(req.url)
-  next()
-})
+  console.log(req.url);
+  next();
+});
 
-  ViteExpress.listen(app, 3000)
+ViteExpress.listen(app, 3000);
