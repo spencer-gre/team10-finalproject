4848f4cecf0bb2a6064015275c9385bd1e900ee7
ctchartier <ctchartier@wpi.edu>
2024-04-22 12:59:44 -0400

    Database Connection?

--- server.js
+++ server.js
@@ -1,7 +1,41 @@
 import express from  'express'
 import ViteExpress from 'vite-express'
 
-const app = express()
+require("dotenv").config();
+const app = express();
+app.use( cookie({
+  name: 'session',
+  keys: ['key1', 'key2'],
+  username: ''
+}))
+
+//Database Code
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASS}@${process.env.HOST}`;
+const { parse } = require("path");
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+let users
+let crosswords
+let boards
+let hangmans
+
+async function run(){
+  await client.connect()
+  const myDatabase = await client.db("final_project")
+  users = myDatabase.collection('Users')
+  crosswords = myDatabase.collection('Crossword')
+  boards = myDatabase.collection('Board')
+  hangmans = myDatabase.collection('Hangman')
+}
+
+run()
 
 app.use( express.json() )
 
