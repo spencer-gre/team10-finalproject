2ef16e01a2cccbd98631948a57f7a779603a51b5
Restyled.io <commits@restyled.io>
2024-04-22 17:00:26 +0000

    Restyled by clang-format

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
-import express from  'express'
+import express from 'express'
 import ViteExpress from 'vite-express'
 
 require("dotenv").config();
 const app = express();
-app.use( cookie({
-  name: 'session',
-  keys: ['key1', 'key2'],
-  username: ''
-}))
+app.use(cookie({name : 'session', keys : [ 'key1', 'key2' ], username : ''}))
 
-//Database Code
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASS}@${process.env.HOST}`;
-const { parse } = require("path");
+// Database Code
+const {MongoClient, ServerApiVersion, ObjectId} = require('mongodb');
+const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASS}@${
+    process.env.HOST}`;
+const {parse} = require("path");
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
+  serverApi : {
+    version : ServerApiVersion.v1,
+    strict : true,
+    deprecationErrors : true,
   }
 });
 
@@ -26,7 +23,7 @@ let crosswords
 let boards
 let hangmans
 
-async function run(){
+async function run() {
   await client.connect()
   const myDatabase = await client.db("final_project")
   users = myDatabase.collection('Users')
@@ -37,11 +34,11 @@ async function run(){
 
 run()
 
-app.use( express.json() )
+app.use(express.json())
 
 app.use( (req,res, next) => {
   console.log( req.url )
   next()
 })
 
-ViteExpress.listen( app, 3000 )
+  ViteExpress.listen(app, 3000)
