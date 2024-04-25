858e5004d915418a920bdd87b511921b8d37b331
spencer <spencerscottg@gmail.com>
2024-04-25 10:57:04 -0400

    init admin page

---
+++ src/Admin.svelte
@@ -0,0 +1,53 @@
+<script>
+  import { onMount } from "svelte";
+  let authUser;
+  
+
+  const getUser = async function () {
+    const response = await fetch("/auth/user", {
+      method: "GET",
+    });
+    let data = await response.json();
+    return data.user;
+  };
+
+  onMount(async () => {
+    let user = await getUser();
+    authUser = user;
+  });
+</script>
+
+
+<main>
+    <body>
+      <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
+        <h5 class="my-0 mr-md-auto font-weight-normal">WPI Games</h5>
+        <nav class="my-2 my-md-0 mr-md-3">
+          <a class="p-2 text-dark" href="#">User: {authUser}</a>
+        </nav>
+        <a class="btn btn-outline-primary" href="../login.html">Log Out</a>
+      </div>
+
+
+     
+
+      
+
+
+      <!-- Bootstrap core JavaScript - Placed at the end of the document so the pages load faster -->
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+      <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+      <script src="../../assets/js/vendor/popper.min.js"></script>
+      <script src="../../dist/js/bootstrap.min.js"></script>
+      <script src="../../assets/js/vendor/holder.min.js"></script>
+      <script>
+        Holder.addTheme('thumb', {
+          bg: '#55595c',
+          fg: '#eceeef',
+          text: 'Thumbnail'
+        });
+      </script>
+    
+
+  </body>
+</main>
