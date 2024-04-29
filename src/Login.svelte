aae64307e2160bb33c8fab2a73d44eda396698e0
Seth Frank <sbfrank@wpi.edu>
2024-04-29 11:49:10 -0400

    Design login page

--- src/Login.svelte
+++ src/Login.svelte
@@ -1,11 +1,19 @@
+<script>
+  import wpi from "./assets/wpi.png";
+  function login() {
+    const html = location.replace("/auth/github");
+  }
+</script>
+
 <main>
   <body class="text-center">
     <!-- <img class="mb-4" src="" alt="" width="72" height="72"> -->
-    <h1 class="h3 mb-3 font-weight-normal">Sign In</h1>
+    <img src={wpi}/>
+    <h1 class="h3 mb-3 font-weight-normal">Welcome to WPI Games!</h1>
 
-    <a
-      aria-describedby="Enables access to the site by signing in with GitHub."
-      href="/auth/github">Login with GitHub</a
-    >
+    <button class="btn btn-lg btn-block btn-primary" on:click={login}>
+      <!-- <a aria-describedby="Enables access to the site by signing in with GitHub." href="/auth/github">Login with GitHub</a> -->
+      Login with GitHub
+    </button>
   </body>
 </main>
